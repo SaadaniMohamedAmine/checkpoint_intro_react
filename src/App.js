54b414d573be626ec78ceb2a7e7fb9bf82cc4e37
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-07 11:33:24 +0200

    Add style to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form.js'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
   return (
-    <div>
-      <Form />
+    <div className="page"
+    style={{backgroundColor: "lightblue"},{}}
+     >
+      <div className="frame">
+        <Form />
+      </div>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Form = () => {
     return (
-        <div class="Contain">
+        
           <form>
              <div class="form-row">
                    <div class="col-md-6 mb-3">
-                        <label for="validationServer01">First name</label>
+                        <label for="validationServer01" className="label">First name</label>
       <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required />
       <div class="valid-feedback">
         Looks good!
       </div>
     </div>
     <div class="col-md-6 mb-3">
-      <label for="validationServer02">Last name</label>
+      <label for="validationServer02" className="label">Last name</label>
       <input type="text" class="form-control is-valid" id="validationServer02" value="Otto" required />
       <div class="valid-feedback">
         Looks good!
@@ -22,14 +22,14 @@ const Form = () => {
   </div>
   <div class="form-row">
     <div class="col-md-6 mb-3">
-      <label for="validationServer03">City</label>
+      <label for="validationServer03" className="label">City</label>
       <input type="text" class="form-control is-invalid" id="validationServer03" aria-describedby="validationServer03Feedback" required />
       <div id="validationServer03Feedback" class="invalid-feedback">
         Please provide a valid city.
       </div>
     </div>
     <div class="col-md-3 mb-3">
-      <label for="validationServer04">State</label>
+      <label for="validationServer04" className="label">State</label>
       <select class="custom-select is-invalid" id="validationServer04" aria-describedby="validationServer04Feedback" required>
         <option selected disabled value="">Choose...</option>
         <option>...</option>
@@ -39,7 +39,7 @@ const Form = () => {
       </div>
     </div>
     <div class="col-md-3 mb-3">
-      <label for="validationServer05">Zip</label>
+      <label for="validationServer05" className="label">Zip</label>
       <input type="text" class="form-control is-invalid" id="validationServer05" aria-describedby="validationServer05Feedback" required />
       <div id="validationServer05Feedback" class="invalid-feedback">
         Please provide a valid zip.
@@ -57,9 +57,9 @@ const Form = () => {
       </div>
     </div>
   </div>
-  <button class="btn btn-primary" type="submit">Submit form</button>
+  <button class="btn btn-primary" type="submit" >Submit form</button>
 </form>
-        </div>
+      
     )
 }
 
